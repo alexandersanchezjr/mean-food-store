7edd77dc34a97d8bd13eae13707b04261eed3891
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-04-19 13:30:44 -0500

    remove: logs in food.controller

--- food-api/src/controller/food.controller.ts
+++ food-api/src/controller/food.controller.ts
@@ -2,38 +2,36 @@ import { Request, Response } from "express";
 import { sample_foods, sample_tags } from "../data";
 
 const getAllFoods = async (req: Request, res: Response) => {
-    console.log(sample_foods);
-    res.send(sample_foods);
-}
+  res.send(sample_foods);
+};
 
 const getFoodByName = async (req: Request, res: Response) => {
-    const foods = sample_foods.filter((food) =>
-        food.name.toLowerCase().includes(req.params.name.toLowerCase())
-      );
-      res.send(foods);
-}
+  const foods = sample_foods.filter((food) =>
+    food.name.toLowerCase().includes(req.params.name.toLowerCase())
+  );
+  res.send(foods);
+};
 
 const getFoodById = async (req: Request, res: Response) => {
-    const food = sample_foods.find((food) => food.id === req.params.foodId);
-    res.send(food);
-}
+  const food = sample_foods.find((food) => food.id === req.params.foodId);
+  res.send(food);
+};
 
 const getAllTags = async (req: Request, res: Response) => {
-    console.log(sample_tags);
-    res.send(sample_tags);
-}
+  res.send(sample_tags);
+};
 
 const getAllFoodsByTag = async (req: Request, res: Response) => {
-    const foods = sample_foods.filter((food) =>
-        food.tags?.includes(req.params.tagName)
-      );
-      res.send(foods);
-}
+  const foods = sample_foods.filter((food) =>
+    food.tags?.includes(req.params.tagName)
+  );
+  res.send(foods);
+};
 
 export {
-    getAllFoods,
-    getFoodByName,
-    getFoodById,
-    getAllTags,
-    getAllFoodsByTag
-}
+  getAllFoods,
+  getFoodByName,
+  getFoodById,
+  getAllTags,
+  getAllFoodsByTag,
+};
