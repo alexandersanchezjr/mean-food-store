07b7da437a5ce7e852cac8eececa610b0a6dd465
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-04-20 07:33:34 -0500

    add: register logic

--- food-api/src/controller/user.controller.ts
+++ food-api/src/controller/user.controller.ts
@@ -3,6 +3,8 @@ import { sample_users } from "../data";
 import jwt from "jsonwebtoken";
 import { UserModel } from "../model/User.model";
 import asyncHandler from "express-async-handler";
+import { HTTP_BAD_REQUEST } from "../constants/http_status";
+import bcrypt from "bcryptjs";
 
 const seedUsers = asyncHandler(async (req: Request, res: Response) => {
   const usersCount = await UserModel.countDocuments();
@@ -15,9 +17,29 @@ const seedUsers = asyncHandler(async (req: Request, res: Response) => {
   res.send("Users created");
 });
 
-const register = async (req: Request, res: Response) => {
-  res.send("register");
-};
+const register = asyncHandler(async (req: Request, res: Response) => {
+  const { name, email, password, address } = req.body;
+  const user = await UserModel.findOne({ email });
+  if (user) {
+    res.status(HTTP_BAD_REQUEST).send("User already exists");
+    return;
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await UserModel.create({
+    id: "",
+    name,
+    email: email.toLowerCase(),
+    password: hashedPassword,
+    address,
+    isAdmin: false,
+  })
+
+  const dbUser = await UserModel.create(newUser);
+
+  res.send(generateTokenResponse(dbUser));
+});
 
 const login = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -26,7 +48,7 @@ const login = asyncHandler(async (req: Request, res: Response) => {
   if (user) {
     res.send(generateTokenResponse(user));
   } else {
-    res.status(400).send("User not found");
+    res.status(HTTP_BAD_REQUEST).send("User not found");
   }
 });
 
